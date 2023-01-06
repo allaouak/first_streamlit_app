73f34049d25a8c22a1b17d76b5e09f5f389f28e8
allaouak <95215513+allaouak@users.noreply.github.com>
2023-01-06 22:54:03 +0100

    Update streamlit_app.py
    add insert query

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,5 @@ st.dataframe(my_data_rows)
 
 s_fruit_choice = st.text_input('What fruit would you like to add?','Kiwi')
 st.write('Thank you for adding ', s_fruit_choice)
-#st.dataframe(s_fruit_choice)
 
-#add_my_fruit = st.text_input('What fruit would you like to add?','Jackfruit') 
-#st.write('The user entered ', add_my_fruit)
+my_cur.execute("insert into fruit_load_list values ('rom streamlit')")
